5abf91c2ee29b38a3459db7b51dff089cd1a8d9f
Hugo Carrah <hugocarrah@gmail.com>
2025-02-27 21:29:55 -0300

    Cadastro de usuário

--- backend/src/auth/controllers/auth.controller.ts
+++ backend/src/auth/controllers/auth.controller.ts
@@ -1,13 +1,22 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth/auth.service';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
-    @HttpCode(HttpStatus.OK)
     @Post('login')
-    signIn(@Body() signInDto: Record<string, any>) {
-        return this.authService.signIn(signInDto.email, signInDto.senha);
+    @ApiBody({
+        schema: {
+            type: 'object',
+            properties: {
+                email: { type: 'string', example: 'usuario@email.com' },
+                senha: { type: 'string', example: '123456' },
+            },
+        },
+    })
+    async login(@Body() body: { email: string, senha: string }) {
+        return this.authService.validateUser(body.email, body.senha);
     }
 }
